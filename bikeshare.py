79a2594fb5a205bbc846a8aa5ed92bf0faaa6a85
Thinsuu <thinyadanarsu@gmail.com>
2025-04-04 21:12:47 +0200

    style: Run VSCode automatic format on bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -8,6 +8,7 @@ CITY_DATA = {
     'washington': 'washington.csv'
 }
 
+
 def get_input(value_name, allowed_values):
     while True:
         value = input(f"Enter a {value_name} ({allowed_values}): ").lower()
@@ -21,31 +22,33 @@ def get_input(value_name, allowed_values):
 def get_filters():
     city_names = list(CITY_DATA.keys())
     city = get_input('city name', city_names)
-    month = get_input('month', ['all', 'january', 'february', 'march', 'april', 'may', 'june'])
-    day = get_input('day of the week', ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
+    month = get_input('month', ['all', 'january',
+                      'february', 'march', 'april', 'may', 'june'])
+    day = get_input('day of the week', [
+                    'all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
 
     print('Hello! Let\'s explore some US bikeshare data!')
 
-    print('-'*40)
+    print('-' * 40)
     return city, month, day
 
 
 def load_data(city, month, day):
     df = pd.read_csv(CITY_DATA[city])
-    
+
     df["Start Time"] = pd.to_datetime(df["Start Time"])
     df["hour"] = df["Start Time"].dt.hour
     df["month"] = df["Start Time"].dt.month
     df["day_of_week"] = df["Start Time"].dt.day_name()
-       
+
     if month != "all":
         months = ["january", "february", "march", "april", "may", "june"]
         month = months.index(month.lower()) + 1
         df = df[df["month"] == month]
-    
+
     if day != "all":
         df = df[df["day_of_week"].str.lower() == day.lower()]
-     
+
     return df
 
 
@@ -57,12 +60,13 @@ def time_stats(df):
     popular_month = df["month"].mode()[0]
     popular_day_of_week = df["day_of_week"].mode()[0]
     popular_hour = df["hour"].mode()[0]
-    
+
     print("Most popular month:", popular_month)
     print("Most popular day of week:", popular_day_of_week)
     print("Most popular start hour:", popular_hour)
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
+
 
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
@@ -77,7 +81,8 @@ def station_stats(df):
     print("\nThe most popular end station: ", popular_end_station)
     print("\nThe most frequent start and end station: ", frequent_combo_start_end)
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print('-' * 40)
+
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
@@ -89,8 +94,8 @@ def trip_duration_stats(df):
     print("\nTotal trip duration in seconds is:", total_time)
     print("\nTotal average time in seconds is:", total_mean_time)
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
- 
+    print('-' * 40)
+
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
@@ -99,14 +104,13 @@ def user_stats(df):
     start_time = time.time()
     user_types_count = df["User Type"].value_counts()
     print("\nUser Type Count is: \n", user_types_count)
-    
+
     if "Gender" in df.columns:
         gender_count = df["Gender"].value_counts()
         print("Gender counts:\n", gender_count)
     else:
         print("Gender data is not available for this city.")
-        
-        
+
     if "Birth Year" in df.columns:
         earliest_birth = df["Birth Year"].min()
         recent_birth = df["Birth Year"].max()
@@ -118,22 +122,25 @@ def user_stats(df):
         print("Birth year data is not available for this city.")
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-    
+    print('-' * 40)
+
+
 def display_data(df):
     """Continuous promt the user and display 5 lines at a time until the user says 'no' or the data is run out"""
     row_index = 0
     while True:
-        display_line_data = input("\nWould you like to see 5 lines of raw data? Enter yes or no\n").lower()
+        display_line_data = input(
+            "\nWould you like to see 5 lines of raw data? Enter yes or no\n").lower()
         if display_line_data == 'yes':
-            print(df.iloc[row_index : row_index + 5])
+            print(df.iloc[row_index: row_index + 5])
             row_index += 5
             if row_index >= len(df):
                 print("There is no data left to show")
                 break
         else:
             break
-    
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -150,4 +157,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+    main()
